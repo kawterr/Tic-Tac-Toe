f4e093788d824625799a69d1ff0e8f40e3da3062
kawterr <69344326+kawterr@users.noreply.github.com>
2023-05-09 20:34:03 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+let turn = 'X';
+let title = document.querySelector('.title')
+let squares = []
+
+function end(num1,num2,num3){
+    title.innerHTML = `${squares[num1]} winner` 
+    document.getElementById('item'+ num1).style.background='#000'
+    document.getElementById('item'+ num2).style.background='#000'
+    document.getElementById('item'+ num3).style.background='#000'
+
+    setInterval(function(){title.innerHTML += '.'},1000)
+    setTimeout(function(){location.reload()},4000)
+
+}
+function winner(){
+    for (let i =1; i<10;i++){
+        squares[i] = document.getElementById('item'+i).innerHTML
+      }
+
+    if (squares[1] == squares[2] && squares[1] == squares[3] && squares[1] != ''){
+        end(1,2,3)
+    }
+
+    else if (squares[4] == squares[5] && squares[4] == squares[6] && squares[4] != ''){
+        end(4,5,6)
+    }
+    else if (squares[7] == squares[8] && squares[7] == squares[9] && squares[7] != ''){
+        end(7,8,9)
+    }
+    else if (squares[1] == squares[4] && squares[1] == squares[7] && squares[1] != ''){
+        end(1,4,7)
+    }
+    else if (squares[2] == squares[5] && squares[2] == squares[8] && squares[8] != ''){
+        end(2,5,8)
+    }
+    else if (squares[3] == squares[6] && squares[3] == squares[9] && squares[9] != ''){
+        end(3,6,9)
+    }
+    else  if (squares[1] == squares[5] && squares[1] == squares[9] && squares[9] != ''){
+        end(1,5,9)
+    }
+    else if (squares[3] == squares[5] && squares[3] == squares[7] && squares[7] != ''){
+        end(3,5,7)
+    }
+
+}
+function game(id){
+
+    let element= document.getElementById(id);
+    if(turn === 'X' && element.innerHTML == ''){
+        element.innerHTML = 'X'
+        turn='O'
+        title.innerHTML = 'O Turn '
+    }
+    else if (turn === 'O' && element.innerHTML ==  ''){
+        element.innerHTML = 'O'
+        title.innerHTML = 'X Turn'
+        turn = 'X'
+    }
+
+    winner()
+    
+}
